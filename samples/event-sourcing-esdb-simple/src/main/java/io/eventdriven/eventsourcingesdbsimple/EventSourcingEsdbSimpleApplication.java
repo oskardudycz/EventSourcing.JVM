54698529df03bcdb236fba54d88dcdc15fc74901
Oskar Dudycz <oskar.dudycz@gmail.com>
2022-03-07 11:44:12 +0100

    Added initial samples structure for Simple Event Sourcing with ESDB

---
+++ samples/event-sourcing-esdb-simple/src/main/java/io/eventdriven/eventsourcingesdbsimple/EventSourcingEsdbSimpleApplication.java
@@ -0,0 +1,13 @@
+package io.eventdriven.eventsourcingesdbsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSourcingEsdbSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventSourcingEsdbSimpleApplication.class, args);
+    }
+
+}
