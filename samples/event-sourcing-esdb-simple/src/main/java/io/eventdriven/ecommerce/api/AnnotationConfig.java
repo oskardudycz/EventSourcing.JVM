a49908d614b6841dfd6fbc30e8d4c0312256cd2a
Oskar Dudycz <oskar.dudycz@gmail.com>
2022-03-09 14:16:56 +0100

    Added basic DI configuration and endpoint to initialize shopping cart

---
+++ samples/event-sourcing-esdb-simple/src/main/java/io/eventdriven/ecommerce/api/AnnotationConfig.java
@@ -0,0 +1,16 @@
+package io.eventdriven.ecommerce.api;
+
+import io.eventdriven.ecommerce.shoppingcarts.initializing.InitializeShoppingCart;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+
+@Configuration
+public class AnnotationConfig {
+  @Bean
+  public Function<InitializeShoppingCart, CompletableFuture<Void>> handleInitializeShoppingCart() {
+    return (command) -> CompletableFuture.allOf();
+  }
+}
--- samples/event-sourcing-esdb-simple/src/main/java/io/eventdriven/ecommerce/api/ECommerceApplication.java
+++ samples/event-sourcing-esdb-simple/src/main/java/io/eventdriven/ecommerce/api/ECommerceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ECommerceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ECommerceApplication.class, args);
     }
-
 }
