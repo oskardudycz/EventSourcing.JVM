9186f9c40d97270e0a7c9451c488ced7f2680cdd
Oskar Dudycz <oskar.dudycz@gmail.com>
2022-07-01 22:24:49 +0200

    Added new example of Events Versioning for RenamedProperty

---
+++ samples/events-versioning/src/test/java/io/eventdriven/eventsversioning/simplemappings/RenamedProperty.java
@@ -0,0 +1,57 @@
+package io.eventdriven.eventsversioning.simplemappings;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.eventdriven.eventsversioning.serialization.Serializer;
+import io.eventdriven.eventsversioning.v1.ShoppingCartEvent;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RenamedProperty {
+  record ShoppingCartOpened(
+    @JsonProperty("shoppingCartId") UUID cartId,
+    UUID clientId
+  ) implements ShoppingCartEvent {
+  }
+
+  @Test
+  public void Should_BeForwardCompatible()
+  {
+    // Given
+    var oldEvent = new ShoppingCartEvent.ShoppingCartOpened(UUID.randomUUID(), UUID.randomUUID());
+    var bytes = Serializer.serialize(oldEvent);
+
+    // When
+    var result = Serializer.deserialize(ShoppingCartOpened.class, bytes);
+
+    // Then
+    assertTrue(result.isPresent());
+
+    var event = result.get();
+
+    assertEquals(oldEvent.shoppingCartId(), event.cartId());
+    assertEquals(oldEvent.clientId(), event.clientId());
+  }
+
+  @Test
+  public void Should_BeBackwardCompatible()
+  {
+    // Given
+    var event = new ShoppingCartOpened(UUID.randomUUID(), UUID.randomUUID());
+    var bytes = Serializer.serialize(event);
+
+    // When
+    var result = Serializer.deserialize(ShoppingCartEvent.ShoppingCartOpened.class, bytes);
+
+    // Then
+    assertTrue(result.isPresent());
+
+    var oldEvent = result.get();
+
+    assertEquals(event.cartId(), oldEvent.shoppingCartId());
+    assertEquals(event.clientId(), oldEvent.clientId());
+  }
+}
