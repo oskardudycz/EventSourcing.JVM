01307134b38dd64c411331ac3d3308b682d1c1a7
Oskar Dudycz <oskar.dudycz@gmail.com>
2022-07-10 21:56:54 +0200

    Added Exercise 08 - Projections

---
+++ workshops/introduction-to-event-sourcing/src/test/java/io/eventdriven/introductiontoeventsourcing/e08_projections_singlestream/tools/EventStore.java
@@ -0,0 +1,51 @@
+package io.eventdriven.introductiontoeventsourcing.e08_projections_singlestream.tools;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+import static io.eventdriven.introductiontoeventsourcing.e08_projections_singlestream.tools.EventEnvelopeBase.EventEnvelope;
+import static io.eventdriven.introductiontoeventsourcing.e08_projections_singlestream.tools.EventEnvelopeBase.EventMetadata;
+
+public class EventStore {
+  private final Map<String, List<Consumer<EventEnvelopeBase>>> handlers = new HashMap<>();
+  private final Map<UUID, List<EventEnvelopeBase>> events = new HashMap<>();
+
+  public <Event> void append(UUID streamId, Event event) {
+    events.compute(streamId, (stream, events) -> {
+      if (events == null)
+        events = new ArrayList<>();
+
+      var eventEnvelope =
+        new EventEnvelope<>(event, EventMetadata.of(events.size(), getCurrentLogPosition()));
+
+      events.add(eventEnvelope);
+
+      var eventHandlers = handlers.get(eventEnvelope.getClass().getTypeName());
+      if (eventHandlers == null)
+        return events;
+
+      for (var handle : eventHandlers) {
+        handle.accept(eventEnvelope);
+      }
+
+      return events;
+    });
+  }
+
+  public <Event> void subscribe(Class<EventEnvelope<Event>> eventClass, Consumer<EventEnvelope<Event>> handler) {
+    handlers.compute(eventClass.getTypeName(), (eventType, consumers) -> {
+      if (consumers == null)
+        consumers = new ArrayList<>();
+
+      consumers.add((event) -> handler.accept(eventClass.cast(event)));
+
+      return consumers;
+    });
+  }
+
+  private int getCurrentLogPosition() {
+    return events.values().stream()
+      .mapToInt(List::size)
+      .sum();
+  }
+}
