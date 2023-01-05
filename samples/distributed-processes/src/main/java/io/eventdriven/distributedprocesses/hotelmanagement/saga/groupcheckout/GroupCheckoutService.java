f91a34553379d49f49335c7ccbe2d7b666f0d222
Oskar Dudycz <oskar.dudycz@gmail.com>
2023-01-05 13:47:49 +0100

    Refactored GroupCheckout from aggregates to entities

--- samples/distributed-processes/src/main/java/io/eventdriven/distributedprocesses/hotelmanagement/saga/groupcheckout/GroupCheckoutService.java
+++ samples/distributed-processes/src/main/java/io/eventdriven/distributedprocesses/hotelmanagement/saga/groupcheckout/GroupCheckoutService.java
@@ -1,6 +1,7 @@
 package io.eventdriven.distributedprocesses.hotelmanagement.saga.groupcheckout;
 
 import io.eventdriven.distributedprocesses.core.aggregates.AggregateStore;
+import io.eventdriven.distributedprocesses.core.entities.CommandHandler;
 import io.eventdriven.distributedprocesses.core.http.ETag;
 
 import java.time.OffsetDateTime;
@@ -9,50 +10,50 @@ import java.util.UUID;
 import static io.eventdriven.distributedprocesses.hotelmanagement.saga.groupcheckout.GroupCheckoutCommand.*;
 
 public class GroupCheckoutService {
-  private final AggregateStore<GroupCheckout, GroupCheckoutEvent, UUID> store;
+  private final CommandHandler<GroupCheckout, GroupCheckoutCommand, GroupCheckoutEvent> commandHandler;
 
-  public GroupCheckoutService(AggregateStore<GroupCheckout, GroupCheckoutEvent, UUID> store) {
-    this.store = store;
+  public GroupCheckoutService(CommandHandler<GroupCheckout, GroupCheckoutCommand, GroupCheckoutEvent> commandHandler) {
+    this.commandHandler = commandHandler;
   }
 
-  public ETag handle(InitiateGroupCheckout command) {
-    return store.add(
-      GroupCheckout.initiate(
-        command.groupCheckoutId(),
-        command.clerkId(),
-        command.guestStayAccountIds(),
-        OffsetDateTime.now()
-      )
-    );
-  }
-
-  public ETag handle(RecordGuestStayInitiation command) {
-    return store.getAndUpdate(
-      current -> current.recordGuestStaysCheckoutInitiation(
-        command.guestStayAccountIds(),
-        OffsetDateTime.now()
-      ),
-      command.groupCheckoutId()
-    );
-  }
-
-  public ETag handle(RecordGuestCheckoutCompletion command) {
-    return store.getAndUpdate(
-      current -> current.recordGuestStayCheckoutCompletion(
-        command.guestStayAccountId(),
-        OffsetDateTime.now()
-      ),
-      command.groupCheckoutId()
-    );
-  }
-
-  public ETag handle(RecordGuestCheckoutFailure command) {
-    return store.getAndUpdate(
-      current -> current.recordGuestStayCheckoutFailure(
-        command.guestStayAccountId(),
-        OffsetDateTime.now()
-      ),
-      command.groupCheckoutId()
-    );
-  }
+//  public ETag handle(InitiateGroupCheckout command) {
+//    return store.add(
+//      GroupCheckout.initiate(
+//        command.groupCheckoutId(),
+//        command.clerkId(),
+//        command.guestStayAccountIds(),
+//        OffsetDateTime.now()
+//      )
+//    );
+//  }
+//
+//  public ETag handle(RecordGuestStayInitiation command) {
+//    return store.getAndUpdate(
+//      current -> current.recordGuestStaysCheckoutInitiation(
+//        command.guestStayAccountIds(),
+//        OffsetDateTime.now()
+//      ),
+//      command.groupCheckoutId()
+//    );
+//  }
+//
+//  public ETag handle(RecordGuestCheckoutCompletion command) {
+//    return store.getAndUpdate(
+//      current -> current.recordGuestStayCheckoutCompletion(
+//        command.guestStayAccountId(),
+//        OffsetDateTime.now()
+//      ),
+//      command.groupCheckoutId()
+//    );
+//  }
+//
+//  public ETag handle(RecordGuestCheckoutFailure command) {
+//    return store.getAndUpdate(
+//      current -> current.recordGuestStayCheckoutFailure(
+//        command.guestStayAccountId(),
+//        OffsetDateTime.now()
+//      ),
+//      command.groupCheckoutId()
+//    );
+//  }
 }
