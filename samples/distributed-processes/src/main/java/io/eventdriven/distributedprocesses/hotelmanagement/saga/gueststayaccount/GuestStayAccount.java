1596be0a07abfdc93e94b2273cef05283932c655
Oskar Dudycz <oskar.dudycz@gmail.com>
2023-01-05 15:48:36 +0100

    Added EventSourcedSpecification base test class. Refactored GuestStayAccount to entity from aggregate

--- samples/distributed-processes/src/main/java/io/eventdriven/distributedprocesses/hotelmanagement/saga/gueststayaccount/GuestStayAccount.java
+++ samples/distributed-processes/src/main/java/io/eventdriven/distributedprocesses/hotelmanagement/saga/gueststayaccount/GuestStayAccount.java
@@ -8,67 +8,68 @@ import java.util.UUID;
 
 import static io.eventdriven.distributedprocesses.hotelmanagement.saga.gueststayaccount.GuestStayAccountEvent.*;
 
-public class GuestStayAccount extends AbstractAggregate<GuestStayAccountEvent, UUID> {
-  private enum Status {
+public record GuestStayAccount(
+  UUID id,
+  Status status,
+  double balance
+) {
+  public enum Status {
     Open,
     Checkout
   }
 
-  private Status status;
-  private double balance;
-
-  public static GuestStayAccount open(UUID guestStayAccountId, OffsetDateTime openedAt) {
-    return new GuestStayAccount(
-      guestStayAccountId,
-      openedAt
-    );
-  }
-
-  private GuestStayAccount(
-    UUID guestStayAccountId,
-    OffsetDateTime openedAt
-  ) {
-    enqueue(new GuestCheckedIn(guestStayAccountId, openedAt));
-  }
-
-  public void recordCharge(double amount, OffsetDateTime now) {
-    if (status != Status.Open)
-      throw new RuntimeException("Cannot record charge if status is other than Open");
-
-    enqueue(new ChargeRecorded(id(), amount, now));
-  }
-
-  public void recordPayment(double amount, OffsetDateTime now) {
-    if (status != Status.Open)
-      throw new RuntimeException("Cannot record payment if status is other than Open");
-
-    enqueue(new PaymentRecorded(id(), amount, now));
-  }
-
-  public void checkout(@Nullable UUID groupCheckoutId, OffsetDateTime now) {
-    if (status != Status.Open || balance != 0) {
-      enqueue(new GuestCheckoutFailed(id(), groupCheckoutId, OffsetDateTime.now()));
-      return;
-    }
-    enqueue(new GuestCheckedOut(id(), groupCheckoutId, now));
-  }
-
-  @Override
-  public void when(GuestStayAccountEvent event) {
-    switch (event) {
-      case GuestCheckedIn opened -> {
-        id = opened.guestStayAccountId();
-        balance = 0;
-        status = Status.Open;
-      }
-      case ChargeRecorded chargeRecorded ->
-        balance -= chargeRecorded.amount();
-      case PaymentRecorded paymentRecorded ->
-        balance += paymentRecorded.amount();
-      case GuestCheckedOut checkoutCompleted ->
-        status = Status.Checkout;
-      case GuestCheckoutFailed ignored -> {
-      }
-    }
-  }
+//  public static GuestStayAccount open(UUID guestStayAccountId, OffsetDateTime openedAt) {
+//    return new GuestStayAccount(
+//      guestStayAccountId,
+//      openedAt
+//    );
+//  }
+//
+//  private GuestStayAccount(
+//    UUID guestStayAccountId,
+//    OffsetDateTime openedAt
+//  ) {
+//    enqueue(new GuestCheckedIn(guestStayAccountId, openedAt));
+//  }
+//
+//  public void recordCharge(double amount, OffsetDateTime now) {
+//    if (status != Status.Open)
+//      throw new RuntimeException("Cannot record charge if status is other than Open");
+//
+//    enqueue(new ChargeRecorded(id(), amount, now));
+//  }
+//
+//  public void recordPayment(double amount, OffsetDateTime now) {
+//    if (status != Status.Open)
+//      throw new RuntimeException("Cannot record payment if status is other than Open");
+//
+//    enqueue(new PaymentRecorded(id(), amount, now));
+//  }
+//
+//  public void checkout(@Nullable UUID groupCheckoutId, OffsetDateTime now) {
+//    if (status != Status.Open || balance != 0) {
+//      enqueue(new GuestCheckoutFailed(id(), groupCheckoutId, OffsetDateTime.now()));
+//      return;
+//    }
+//    enqueue(new GuestCheckedOut(id(), groupCheckoutId, now));
+//  }
+//
+//  @Override
+//  public void when(GuestStayAccountEvent event) {
+//    switch (event) {
+//      case GuestCheckedIn opened -> {
+//        id = opened.guestStayAccountId();
+//        balance = 0;
+//        status = Status.Open;
+//      }
+//      case ChargeRecorded chargeRecorded ->
+//        balance -= chargeRecorded.amount();
+//      case PaymentRecorded paymentRecorded ->
+//        balance += paymentRecorded.amount();
+//      case GuestCheckedOut checkoutCompleted ->
+//        status = Status.Checkout;
+//      case GuestCheckoutFailed ignored -> {
+//      }
+//    }
+//  }
 }
--- samples/distributed-processes/src/main/java/io/eventdriven/distributedprocesses/hotelmanagement/saga/gueststayaccount/GuestStayAccountService.java
+++ samples/distributed-processes/src/main/java/io/eventdriven/distributedprocesses/hotelmanagement/saga/gueststayaccount/GuestStayAccountService.java
@@ -1,9 +1,13 @@
 package io.eventdriven.distributedprocesses.hotelmanagement.saga.gueststayaccount;
 
 import io.eventdriven.distributedprocesses.core.aggregates.AggregateStore;
+import io.eventdriven.distributedprocesses.core.entities.CommandHandler;
 import io.eventdriven.distributedprocesses.core.events.EventBus;
 import io.eventdriven.distributedprocesses.core.http.ETag;
 import io.eventdriven.distributedprocesses.core.retries.RetryPolicy;
+import io.eventdriven.distributedprocesses.hotelmanagement.saga.groupcheckout.GroupCheckout;
+import io.eventdriven.distributedprocesses.hotelmanagement.saga.groupcheckout.GroupCheckoutCommand;
+import io.eventdriven.distributedprocesses.hotelmanagement.saga.groupcheckout.GroupCheckoutEvent;
 import io.eventdriven.distributedprocesses.hotelmanagement.saga.gueststayaccount.GuestStayAccountCommand.CheckOutGuest;
 import io.eventdriven.distributedprocesses.hotelmanagement.saga.gueststayaccount.GuestStayAccountCommand.CheckInGuest;
 import io.eventdriven.distributedprocesses.hotelmanagement.saga.gueststayaccount.GuestStayAccountCommand.RecordCharge;
@@ -13,59 +17,53 @@ import java.time.OffsetDateTime;
 import java.util.UUID;
 
 public class GuestStayAccountService {
-  private final AggregateStore<GuestStayAccount, GuestStayAccountEvent, UUID> store;
-  private final EventBus eventBus;
-  private final RetryPolicy retryPolicy;
+  private final CommandHandler<GroupCheckout, GroupCheckoutCommand, GroupCheckoutEvent> commandHandler;
 
   public GuestStayAccountService(
-    AggregateStore<GuestStayAccount, GuestStayAccountEvent, UUID> store,
-    EventBus eventBus,
-    RetryPolicy retryPolicy
+    CommandHandler<GroupCheckout, GroupCheckoutCommand, GroupCheckoutEvent> commandHandler
   ) {
-    this.store = store;
-    this.eventBus = eventBus;
-    this.retryPolicy = retryPolicy;
+    this.commandHandler = commandHandler;
   }
-
-  public ETag handle(CheckInGuest command) {
-    return store.add(
-      GuestStayAccount.open(
-        command.guestStayAccountId(),
-        OffsetDateTime.now()
-      )
-    );
-  }
-
-  public ETag handle(RecordCharge command) {
-    return store.getAndUpdate(
-      current -> current.recordCharge(
-        command.amount(),
-        OffsetDateTime.now()
-      ),
-      command.guestStayAccountId()
-    );
-  }
-
-  public ETag handle(RecordPayment command) {
-    return store.getAndUpdate(
-      current -> current.recordPayment(
-        command.amount(),
-        OffsetDateTime.now()
-      ),
-      command.guestStayAccountId()
-    );
-  }
-
-  public ETag handle(CheckOutGuest command) {
-    return retryPolicy.run(ack -> {
-      var result = store.getAndUpdate(
-        current -> current.checkout(
-          command.guestStayAccountId(),
-          OffsetDateTime.now()
-        ),
-        command.guestStayAccountId()
-      );
-      ack.accept(result);
-    });
-  }
+//
+//  public ETag handle(CheckInGuest command) {
+//    return store.add(
+//      GuestStayAccount.open(
+//        command.guestStayAccountId(),
+//        OffsetDateTime.now()
+//      )
+//    );
+//  }
+//
+//  public ETag handle(RecordCharge command) {
+//    return store.getAndUpdate(
+//      current -> current.recordCharge(
+//        command.amount(),
+//        OffsetDateTime.now()
+//      ),
+//      command.guestStayAccountId()
+//    );
+//  }
+//
+//  public ETag handle(RecordPayment command) {
+//    return store.getAndUpdate(
+//      current -> current.recordPayment(
+//        command.amount(),
+//        OffsetDateTime.now()
+//      ),
+//      command.guestStayAccountId()
+//    );
+//  }
+//
+//  public ETag handle(CheckOutGuest command) {
+//    return retryPolicy.run(ack -> {
+//      var result = store.getAndUpdate(
+//        current -> current.checkout(
+//          command.guestStayAccountId(),
+//          OffsetDateTime.now()
+//        ),
+//        command.guestStayAccountId()
+//      );
+//      ack.accept(result);
+//    });
+//  }
 }
