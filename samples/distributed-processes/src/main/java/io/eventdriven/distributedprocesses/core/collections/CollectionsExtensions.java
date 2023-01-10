3e0a285d315811d3c4e1589bbcbfbc6335ff050b
Oskar Dudycz <oskar.dudycz@gmail.com>
2023-01-10 12:26:43 +0100

    Finished GuestStayAccountService implementation

---
+++ samples/distributed-processes/src/main/java/io/eventdriven/distributedprocesses/core/collections/CollectionsExtensions.java
@@ -0,0 +1,7 @@
+package io.eventdriven.distributedprocesses.core.collections;
+
+public final class CollectionsExtensions {
+  public static <T> T[] toArray(T...elements){
+    return elements;
+  }
+}
--- samples/distributed-processes/src/main/java/io/eventdriven/distributedprocesses/hotelmanagement/saga/gueststayaccount/GuestStayAccountService.java
+++ samples/distributed-processes/src/main/java/io/eventdriven/distributedprocesses/hotelmanagement/saga/gueststayaccount/GuestStayAccountService.java
@@ -1,69 +1,47 @@
 package io.eventdriven.distributedprocesses.hotelmanagement.saga.gueststayaccount;
 
-import io.eventdriven.distributedprocesses.core.aggregates.AggregateStore;
-import io.eventdriven.distributedprocesses.core.entities.CommandHandler;
-import io.eventdriven.distributedprocesses.core.events.EventBus;
+import io.eventdriven.distributedprocesses.core.entities.EntityStore;
 import io.eventdriven.distributedprocesses.core.http.ETag;
 import io.eventdriven.distributedprocesses.core.retries.RetryPolicy;
-import io.eventdriven.distributedprocesses.hotelmanagement.saga.groupcheckout.GroupCheckout;
-import io.eventdriven.distributedprocesses.hotelmanagement.saga.groupcheckout.GroupCheckoutCommand;
-import io.eventdriven.distributedprocesses.hotelmanagement.saga.groupcheckout.GroupCheckoutEvent;
-import io.eventdriven.distributedprocesses.hotelmanagement.saga.gueststayaccount.GuestStayAccountCommand.CheckOutGuest;
-import io.eventdriven.distributedprocesses.hotelmanagement.saga.gueststayaccount.GuestStayAccountCommand.CheckInGuest;
-import io.eventdriven.distributedprocesses.hotelmanagement.saga.gueststayaccount.GuestStayAccountCommand.RecordCharge;
-import io.eventdriven.distributedprocesses.hotelmanagement.saga.gueststayaccount.GuestStayAccountCommand.RecordPayment;
+import io.eventdriven.distributedprocesses.hotelmanagement.saga.gueststayaccount.GuestStayAccountCommand.*;
 
-import java.time.OffsetDateTime;
 import java.util.UUID;
 
 public class GuestStayAccountService {
-  private final CommandHandler<GroupCheckout, GroupCheckoutCommand, GroupCheckoutEvent> commandHandler;
+  private final EntityStore<GuestStayAccount, GuestStayAccountEvent> store;
+  private RetryPolicy retryPolicy;
 
   public GuestStayAccountService(
-    CommandHandler<GroupCheckout, GroupCheckoutCommand, GroupCheckoutEvent> commandHandler
+    EntityStore<GuestStayAccount, GuestStayAccountEvent> store,
+    RetryPolicy retryPolicy
   ) {
-    this.commandHandler = commandHandler;
+    this.store = store;
+    this.retryPolicy = retryPolicy;
   }
-//
-//  public ETag handle(CheckInGuest command) {
-//    return store.add(
-//      GuestStayAccount.open(
-//        command.guestStayAccountId(),
-//        OffsetDateTime.now()
-//      )
-//    );
-//  }
-//
-//  public ETag handle(RecordCharge command) {
-//    return store.getAndUpdate(
-//      current -> current.recordCharge(
-//        command.amount(),
-//        OffsetDateTime.now()
-//      ),
-//      command.guestStayAccountId()
-//    );
-//  }
-//
-//  public ETag handle(RecordPayment command) {
-//    return store.getAndUpdate(
-//      current -> current.recordPayment(
-//        command.amount(),
-//        OffsetDateTime.now()
-//      ),
-//      command.guestStayAccountId()
-//    );
-//  }
-//
-//  public ETag handle(CheckOutGuest command) {
-//    return retryPolicy.run(ack -> {
-//      var result = store.getAndUpdate(
-//        current -> current.checkout(
-//          command.guestStayAccountId(),
-//          OffsetDateTime.now()
-//        ),
-//        command.guestStayAccountId()
-//      );
-//      ack.accept(result);
-//    });
-//  }
+
+  public ETag handle(CheckInGuest command) {
+    return handle(command.guestStayAccountId(), command);
+  }
+
+  public ETag handle(RecordCharge command) {
+    return handle(command.guestStayAccountId(), command);
+  }
+
+  public ETag handle(RecordPayment command) {
+    return handle(command.guestStayAccountId(), command);
+  }
+
+  public ETag handle(CheckOutGuest command) {
+    return retryPolicy.run(ack -> {
+      var result = handle(command.guestStayAccountId(), command);
+      ack.accept(result);
+    });
+  }
+
+  private ETag handle(UUID id, GuestStayAccountCommand command) {
+    return store.getAndUpdate(
+      (state) -> GuestStayAccountDecider.handle(command, state),
+      id
+    );
+  }
 }
