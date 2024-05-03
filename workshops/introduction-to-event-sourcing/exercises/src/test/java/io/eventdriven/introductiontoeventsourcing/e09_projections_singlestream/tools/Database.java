e732edb5de90186b190269cbcd4c6591126756f2
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-05-03 21:40:51 +0200

    Renamed projection exercices to have monotinic numbering

---
+++ workshops/introduction-to-event-sourcing/exercises/src/test/java/io/eventdriven/introductiontoeventsourcing/e09_projections_singlestream/tools/Database.java
@@ -0,0 +1,53 @@
+package io.eventdriven.introductiontoeventsourcing.e09_projections_singlestream.tools;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+public class Database {
+  private final Map<String, Object> storage = new HashMap<>();
+
+  private static final ObjectMapper mapper =
+    new JsonMapper()
+      .registerModule(new JavaTimeModule())
+      .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+      .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+      .setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+
+  public <T> void store(Class<T> typeClass, UUID id, Object obj) {
+    storage.compute(getId(typeClass, id), (ignore, value) -> obj);
+  }
+
+  public <T> void delete(Class<T> typeClass, UUID id) {
+    storage.remove(getId(typeClass, id));
+  }
+
+  public <T> Optional<T> get(Class<T> typeClass, UUID id) {
+    return Optional.ofNullable(
+      typeClass.cast(
+        storage.compute(getId(typeClass, id), (k, v) ->
+        {
+          try {
+            return v != null ?
+              mapper.readValue(mapper.writeValueAsString(v), typeClass)
+              : null;
+          } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+          }
+        })));
+  }
+
+  private static <T> String getId(Class<T> typeClass, UUID id) {
+    return "%s-%s".formatted(typeClass.getTypeName(), id);
+  }
+}
