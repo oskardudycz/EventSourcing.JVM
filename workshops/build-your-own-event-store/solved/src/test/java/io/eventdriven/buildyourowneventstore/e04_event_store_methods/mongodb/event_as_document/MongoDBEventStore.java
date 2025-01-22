9bbbaa07e1ff9d69a04919bac113e16760034f75
Oskar Dudycz <oskar.dudycz@gmail.com>
2025-01-22 11:39:16 +0100

    Added transaction to MongoDB event store with event per document

--- workshops/build-your-own-event-store/solved/src/test/java/io/eventdriven/buildyourowneventstore/e04_event_store_methods/mongodb/event_as_document/MongoDBEventStore.java
+++ workshops/build-your-own-event-store/solved/src/test/java/io/eventdriven/buildyourowneventstore/e04_event_store_methods/mongodb/event_as_document/MongoDBEventStore.java
@@ -15,7 +15,6 @@ import io.eventdriven.buildyourowneventstore.e04_event_store_methods.mongodb.eve
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.IntStream;
@@ -58,87 +57,87 @@ public class MongoDBEventStore implements EventStore {
     var streamId = streamName.streamId();
     var streamNameValue = streamName.toString();
 
-    // Resolve collection
+    // Resolve collections
     var streamsCollection = streamsCollection();
     var eventsCollection = eventsCollection();
 
     var now = LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS);
 
-    long currentVersion;
+    mongoClient.startSession().withTransaction(() -> {
+      long currentVersion;
+      if (expectedVersion != null) {
+        currentVersion = expectedVersion;
+      } else {
+        var stream = streamsCollection.find(Filters.eq("streamName", streamNameValue))
+          .projection(Projections.include("metadata.streamPosition"))
+          .first();
 
-    if (expectedVersion != null) {
-      currentVersion = expectedVersion;
-    } else {
-      var stream = streamsCollection.find(Filters.eq("streamName", streamNameValue))
-        .projection(Projections.include("metadata.streamPosition"))
-        .first();
+        currentVersion = stream != null ?
+          stream.metadata().streamPosition()
+          : 0L;
+      }
 
-      currentVersion = stream != null ?
-        stream.metadata().streamPosition()
-        : 0L;
-    }
+      var envelopes = IntStream.range(0, events.length)
+        .mapToObj(index -> {
+          var event = events[index];
 
-    var envelopes = IntStream.range(0, events.length)
-      .mapToObj(index -> {
-        var event = events[index];
+          return EventEnvelope.of(
+            event,
+            new EventMetadata(
+              UUID.randomUUID().toString(),
+              eventTypeMapper.toName(event.getClass()),
+              currentVersion + index + 1,
+              streamNameValue
+            ),
+            eventDataCodec
+          );
+        }).toList();
 
-        return EventEnvelope.of(
-          event,
-          new EventMetadata(
-            UUID.randomUUID().toString(),
-            eventTypeMapper.toName(event.getClass()),
-            currentVersion + index + 1,
-            streamNameValue
-          ),
-          eventDataCodec
-        );
-      }).toList();
+      // Append events upserting the document
+      var streamUpdateResult = streamsCollection.updateOne(
+        Filters.and(
+          Filters.eq("streamName", streamNameValue),
+          Filters.eq("metadata.streamPosition", currentVersion)
+        ),
+        Updates.combine(
+          // Append events
+          // Increment stream position
+          Updates.inc("metadata.streamPosition", events.length),
+          // Set default metadata on insert
+          Updates.setOnInsert("streamName", streamNameValue),
+          Updates.setOnInsert("metadata.streamId", streamId),
+          Updates.setOnInsert("metadata.streamType", streamType),
+          Updates.setOnInsert("metadata.createdAt", now),
+          // Update metadata
+          Updates.set("metadata.updatedAt", now)
+        ),
+        upsert
+      );
 
-    // Append events upserting the document
-    var streamUpdateResult = streamsCollection.updateOne(
-      Filters.and(
-        Filters.eq("streamName", streamNameValue),
-        Filters.eq("metadata.streamPosition", currentVersion)
-      ),
-      Updates.combine(
-        // Append events
-        // Increment stream position
-        Updates.inc("metadata.streamPosition", events.length),
-        // Set default metadata on insert
-        Updates.setOnInsert("streamName", streamNameValue),
-        Updates.setOnInsert("metadata.streamId", streamId),
-        Updates.setOnInsert("metadata.streamType", streamType),
-        Updates.setOnInsert("metadata.createdAt", now),
-        // Update metadata
-        Updates.set("metadata.updatedAt", now)
-      ),
-      upsert
-    );
+      if (streamUpdateResult.getModifiedCount() == 0L && streamUpdateResult.getUpsertedId() == null)
+        throw new IllegalStateException("Expected version did not match the stream version!");
 
-    if (streamUpdateResult.getModifiedCount() == 0L && streamUpdateResult.getUpsertedId() == null)
-      throw new IllegalStateException("Expected version did not match the stream version!");
+      var eventAppendResult = eventsCollection.insertMany(
+        envelopes
+      );
 
-    var eventAppendResult = eventsCollection.insertMany(
-      envelopes
-    );
+      if (eventAppendResult.getInsertedIds().size() != envelopes.size())
+        throw new IllegalStateException("Expected version did not match the stream version!");
 
-    if (eventAppendResult.getInsertedIds().size() != envelopes.size())
-      throw new IllegalStateException("Expected version did not match the stream version!");
+      return true;
+    });
   }
 
   @Override
   public List<Object> getEvents(StreamName streamName, Long atStreamVersion, LocalDateTime atTimestamp) {
-    // Resolve collection
     var eventsCollection = eventsCollection();
 
-    // Read events from the stream document
-    var events = eventsCollection.find(Filters.eq("metadata.streamName", streamName.toString())).into(new ArrayList<>());
-
-    return events != null ?
-      events.stream().map(eventEnvelope ->
-        eventEnvelope.getEvent(eventDataCodec)
-      ).toList()
-      : Collections.emptyList();
+    return eventsCollection
+      .find(Filters.eq("metadata.streamName", streamName.toString()))
+      .into(new ArrayList<>())
+      .stream()
+      .map(eventEnvelope -> eventEnvelope.getEvent(eventDataCodec))
+      .toList();
   }
 
   private MongoCollection<EventStream> streamsCollection() {
