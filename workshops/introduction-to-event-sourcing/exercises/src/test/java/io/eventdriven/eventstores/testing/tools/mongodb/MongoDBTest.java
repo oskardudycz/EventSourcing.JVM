9b9e8dcc2c665abc2d11c7a2689968ccc80f932d
Oskar Dudycz <oskar.dudycz@gmail.com>
2025-01-31 20:07:06 +0100

    Added PostgreSQL and MongoDB stores implementations to introduction workshop

---
+++ workshops/introduction-to-event-sourcing/exercises/src/test/java/io/eventdriven/eventstores/testing/tools/mongodb/MongoDBTest.java
@@ -0,0 +1,45 @@
+package io.eventdriven.eventstores.testing.tools.mongodb;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import io.eventdriven.eventstores.mongodb.config.NativeMongoConfig;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public abstract class MongoDBTest {
+  protected static MongoClient mongoClient;
+  private static final int maxDatabaseLength = 63;
+
+  @BeforeAll
+  public void setupConnection() {
+    mongoClient = NativeMongoConfig.createClient();
+  }
+
+  protected MongoDatabase getFreshDatabase() {
+    var databaseName = StackWalker
+      .getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE)
+      .getCallerClass()
+      .getName()
+      .replace("io.eventdriven.buildyourowneventstore", "")
+      .replace(".", "-")
+      .toLowerCase();
+
+    if (databaseName.length() >= maxDatabaseLength)
+      databaseName = databaseName.substring(0, maxDatabaseLength);
+
+    return getFreshDatabase(databaseName);
+  }
+
+  protected MongoDatabase getFreshDatabase(String databaseName) {
+    mongoClient.getDatabase(databaseName).drop();
+
+    return mongoClient.getDatabase(databaseName);
+  }
+
+  @AfterAll
+  public void tearDown() {
+    mongoClient.close();
+  }
+}
