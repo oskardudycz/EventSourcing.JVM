9876538091fdc7fd5e8987fb74721b1a658987ff
Oskar Dudycz <oskar.dudycz@gmail.com>
2025-02-01 10:50:59 +0100

    Copy and pasted ESDB solutions for MongoDB as template

---
+++ workshops/introduction-to-event-sourcing/solved/src/test/java/io/eventdriven/introductiontoeventsourcing/e08_optimistic_concurrency/mongodb/mixed/app/package-info.java
@@ -0,0 +1,6 @@
+@NonNullFields
+@NonNullApi
+package io.eventdriven.introductiontoeventsourcing.e08_optimistic_concurrency.mongodb.mixed.app;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
---
+++ workshops/introduction-to-event-sourcing/solved/src/test/java/io/eventdriven/introductiontoeventsourcing/e08_optimistic_concurrency/mongodb/mixed/app/shoppingcarts/ShoppingCartStore.java
@@ -0,0 +1,57 @@
+package io.eventdriven.introductiontoeventsourcing.e08_optimistic_concurrency.mongodb.mixed.app.shoppingcarts;
+
+import io.eventdriven.introductiontoeventsourcing.e07_application_logic.mongodb.core.functional.Tuple;
+import io.eventdriven.introductiontoeventsourcing.e08_optimistic_concurrency.mongodb.core.eventStoreDB.EventStore;
+import io.eventdriven.introductiontoeventsourcing.e08_optimistic_concurrency.mongodb.core.http.ETag;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.function.Function;
+
+public class ShoppingCartStore {
+  private final EventStore eventStore;
+
+  public ShoppingCartStore(EventStore eventStore) {
+    this.eventStore = eventStore;
+  }
+
+  public Optional<Tuple<ShoppingCart, ETag>> get(UUID id) {
+    return eventStore.aggregateStream(
+        ShoppingCartEvent.class,
+        (state, event) -> {
+          state.evolve(event);
+          return state;
+        },
+        ShoppingCart::initial,
+        toStreamName(id)
+      )
+      .map(r -> new Tuple<>(r.first(), ETag.weak(r.second())));
+  }
+
+  public ETag add(UUID id, ShoppingCartEvent event) {
+    return ETag.weak(
+      eventStore.add(toStreamName(id), new Object[]{event})
+    );
+  }
+
+  public ETag getAndUpdate(UUID id, ETag eTag, Function<ShoppingCart, ShoppingCartEvent> handle) {
+    return ETag.weak(
+      eventStore.getAndUpdate(
+        ShoppingCartEvent.class,
+        (state, event) -> {
+          state.evolve(event);
+          return state;
+        },
+        ShoppingCart::initial,
+        toStreamName(id),
+        eTag.toLong(),
+        (state) -> List.of(handle.apply(state))
+      )
+    );
+  }
+
+  private String toStreamName(UUID id) {
+    return "shopping_cart-" + id.toString();
+  }
+}
