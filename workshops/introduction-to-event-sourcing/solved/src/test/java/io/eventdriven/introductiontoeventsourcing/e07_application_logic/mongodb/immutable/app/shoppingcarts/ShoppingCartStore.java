a86f9c72b1e7cfe9bf600bdbb8f6e2c7e8aa2507
Oskar Dudycz <oskar.dudycz@gmail.com>
2025-02-01 10:50:59 +0100

    Copy and pasted ESDB solutions for MongoDB as template

---
+++ workshops/introduction-to-event-sourcing/solved/src/test/java/io/eventdriven/introductiontoeventsourcing/e07_application_logic/mongodb/immutable/app/shoppingcarts/ShoppingCartStore.java
@@ -0,0 +1,43 @@
+package io.eventdriven.introductiontoeventsourcing.e07_application_logic.mongodb.immutable.app.shoppingcarts;
+
+import io.eventdriven.introductiontoeventsourcing.e07_application_logic.mongodb.core.eventStoreDB.EventStore;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.function.Function;
+
+public class ShoppingCartStore {
+  private final EventStore eventStore;
+
+  public ShoppingCartStore(EventStore eventStore) {
+    this.eventStore = eventStore;
+  }
+
+  public Optional<ShoppingCart> get(UUID id) {
+    return eventStore.aggregateStream(
+      ShoppingCartEvent.class,
+      ShoppingCart::evolve,
+      ShoppingCart::initial,
+      toStreamName(id)
+    );
+  }
+
+  public void add(UUID id, ShoppingCartEvent event) {
+    eventStore.add(toStreamName(id), new Object[]{event});
+  }
+
+  public void getAndUpdate(UUID id, Function<ShoppingCart, ShoppingCartEvent> handle) {
+    eventStore.getAndUpdate(
+      ShoppingCartEvent.class,
+      ShoppingCart::evolve,
+      ShoppingCart::initial,
+      toStreamName(id),
+      (state) -> List.of(handle.apply(state))
+    );
+  }
+
+  private String toStreamName(UUID id) {
+    return "shopping_cart-" + id.toString();
+  }
+}
---
+++ workshops/introduction-to-event-sourcing/solved/src/test/java/io/eventdriven/introductiontoeventsourcing/e08_optimistic_concurrency/mongodb/mutable/app/shoppingcarts/ShoppingCartStore.java
@@ -0,0 +1,48 @@
+package io.eventdriven.introductiontoeventsourcing.e08_optimistic_concurrency.mongodb.mutable.app.shoppingcarts;
+
+import io.eventdriven.introductiontoeventsourcing.e07_application_logic.mongodb.core.functional.Tuple;
+import io.eventdriven.introductiontoeventsourcing.e08_optimistic_concurrency.mongodb.core.eventStoreDB.EventStore;
+import io.eventdriven.introductiontoeventsourcing.e08_optimistic_concurrency.mongodb.core.http.ETag;
+
+import java.util.Optional;
+import java.util.UUID;
+import java.util.function.Consumer;
+
+public class ShoppingCartStore {
+  private final EventStore eventStore;
+
+  public ShoppingCartStore(EventStore eventStore) {
+    this.eventStore = eventStore;
+  }
+
+  public Optional<Tuple<ShoppingCart, ETag>> get(UUID id) {
+    return eventStore.aggregateStream(
+        ShoppingCartEvent.class,
+        ShoppingCart::initial,
+        toStreamName(id)
+      )
+      .map(r -> new Tuple<>(r.first(), ETag.weak(r.second())));
+  }
+
+  public ETag add(UUID id, ShoppingCart shoppingCart) {
+    return ETag.weak(
+      eventStore.add(toStreamName(id), shoppingCart)
+    );
+  }
+
+  public ETag getAndUpdate(UUID id, ETag eTag, Consumer<ShoppingCart> handle) {
+    return ETag.weak(
+      eventStore.getAndUpdate(
+        ShoppingCartEvent.class,
+        ShoppingCart::initial,
+        toStreamName(id),
+        eTag.toLong(),
+        handle
+      )
+    );
+  }
+
+  private String toStreamName(UUID id) {
+    return "shopping_cart-" + id.toString();
+  }
+}
