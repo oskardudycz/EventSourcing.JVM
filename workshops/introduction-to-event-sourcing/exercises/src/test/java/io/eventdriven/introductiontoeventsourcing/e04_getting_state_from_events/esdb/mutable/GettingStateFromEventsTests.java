1c32296a46a74b2015bb31ec5f90f80553f8ec8d
Oskar Dudycz <oskar.dudycz@gmail.com>
2025-02-04 07:27:20 +0100

    Added serializer to ESDB append and get solution

--- workshops/introduction-to-event-sourcing/exercises/src/test/java/io/eventdriven/introductiontoeventsourcing/e04_getting_state_from_events/esdb/mutable/GettingStateFromEventsTests.java
+++ workshops/introduction-to-event-sourcing/exercises/src/test/java/io/eventdriven/introductiontoeventsourcing/e04_getting_state_from_events/esdb/mutable/GettingStateFromEventsTests.java
@@ -165,6 +165,8 @@ public class GettingStateFromEventsTests extends EventStoreDBTest {
     Canceled
   }
 
+  private ESDBSerializer serializer = new ESDBSerializer();
+  
   static ShoppingCart getShoppingCart(EventStoreDBClient eventStore, String streamName) {
     // 1. Add logic here
     throw new RuntimeException("Not implemented!");
